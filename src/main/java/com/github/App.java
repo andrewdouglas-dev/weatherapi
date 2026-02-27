aa56ca4aa1205444c7915e2bbe0115dca4e5735d
andrewdouglas-dev <andrew.r.douglas@outlook.com>
2026-02-26 20:46:18 -0600

    Updated to gracefully shutdown the server. Changed port to be dynamically loaded from .env variable.

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -2,21 +2,28 @@ package com.github;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.logging.Logger;
 
 import com.github.handlers.WeatherHandler;
 import com.sun.net.httpserver.HttpServer;
 
-public class App 
-{
-    public static void main( String[] args ) throws IOException
-    {
-        int port = 8080;
+public class App {
+    private final static Logger logger = Logger.getLogger(App.class.getName());
+    public static void main( String[] args ) throws IOException {
+        String port = System.getenv("PORT");
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(port)), 0);
 
         server.createContext("/api/v1/weather", new WeatherHandler());
 
         server.setExecutor(null);
         server.start();
+
+        logger.info("Server started on http://localhost:" + port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutting down server");
+            server.stop(0);
+        }));
     }
 }
